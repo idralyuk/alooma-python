f468303df315d307d7fc85a8c07fc0d969555775
Watchdog user <watchdog@alooma.io>
2017-03-01 14:25:27 +0000

    "Updated alooma-python version from 0.1.36 to 0.1.37"

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 from distutils.core import setup
 
 setup(name='alooma',
-      version='0.1.36',
+      version='0.1.37',
       description='Alooma python API',
       author='Yonatan Kiron',
       author_email='yonatan@alooma.io',
